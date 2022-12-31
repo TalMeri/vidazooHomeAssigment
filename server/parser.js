7310391c6134da53a4fb7eb3b903130e1db379ad
TalMeri <tal.meridor@gmail.com>
2022-12-31 22:42:22 +0200

    remove rows from parser

--- server/parser.js
+++ server/parser.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const parser_utils = require("./utils/parser_utils");
-const NodeCache = require('node-cache');
-const myCache = new NodeCache();
 
 router.get("/getCount", async (req, res, next) => {
     try {
